09c12996f681b08d67b76280ca9c4dd677e1aeae
Dhruv Rathore <dhruv.rathore99@gmail.com>
2022-02-28 04:34:37 +0530

    refactor : v1.0
    - link fix
    - installation
    - component curation
    - error handling
    - files + repo structure

--- components/modal/modal.js
+++ components/modal/modal.js
@@ -1,11 +1,11 @@
-const modalDiv = document.querySelector(".modal");
+const modalDiv = document.querySelector("#modal-popup");
 const modalCloseBtn = document.querySelector("#modal-close-btn");
 const modalOpenBtn = document.querySelector("#modal-open-btn");
 
 modalCloseBtn.addEventListener("click", () => {
-  modalDiv.classList.add("visible");
+  modalDiv.classList.remove("visible");
 });
 
 modalOpenBtn.addEventListener("click", () => {
-  modalDiv.classList.remove("visible");
+  modalDiv.classList.add("visible");
 });
